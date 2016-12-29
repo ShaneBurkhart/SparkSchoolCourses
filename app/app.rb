c724c4038187e160ccc0bb1fee9068f9a7603474
Shane Burkhart <shaneburkhart@gmail.com>
2016-12-29 17:07:54 -0600

    Add payments endpoint.

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,20 @@
 #require 'sinatra'
 require 'stripe'
 
-puts ENV['STRIPE_SECRET']
+Stripe.api_key = ENV['STRIPE_SECRET']
+
+get '/payments' do
+    token = params[:stripeToken]
+
+    begin
+        charge = Stripe::Charge.create(
+            :amount => 29900,
+            :currency => "usd",
+            :source => token,
+            :description => "Presell purchase of the 6 week SaaS web app course."
+        )
+        redirect "https://trysparkschool.com/saas-success"
+    rescue Stripe::CardError => e
+        redirect "https://trysparkschool.com/saas-declined"
+    end
+end
